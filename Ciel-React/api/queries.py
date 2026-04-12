f9e2074288a4de707cca72c9ca080e2b63a57d93
Oussama <Hassouneoussama8@gmail.com>
2026-04-12 20:32:31 +1000

    split backend logic to many files

--- Ciel-React/api/queries.py
+++ Ciel-React/api/queries.py
@@ -15,89 +15,6 @@ journal_codes= journal_achat + journal_tresorie + journal_report_nouveau
 custom_condition = [{'44111041':90},{'44111021':120}]
 
 
-def filter_supplier_condition(num_supplier):
-    for supplier in custom_condition:
-        for key, value in supplier.items():
-            if num_supplier == key:
-                return value
-    return 30
-
-def month_delay(due_date, payment_date, end_date):
-    if pd.notna(payment_date):
-        return ((payment_date.year - due_date.year)*12 + (payment_date.month - due_date.month) + 1)
-    else:
-        return ((end_date.year - due_date.year)*12 + (end_date.month - due_date.month) +1 )
-    
-
-def clean_excel(current_year_file, past_year_file, excluded_suppliers):
-    cleaned_rows_current_year = []
-    cleaned_rows_past_year = []
-    current_supplier_number = None
-    current_supplier_name = None
-    current_payement_condition = None
-
-    df_raw_current_year = pd.read_excel(current_year_file, header=None)
-    for _, row in df_raw_current_year.iterrows():
-            
-        first_cell = str(row[0]).strip()
-
-
-        if first_cell.startswith('4411'):
-            current_supplier_number = first_cell.split()[0]
-            current_supplier_name = ' '.join(first_cell.split()[1:])
-            current_payement_condition = filter_supplier_condition(current_supplier_number)
-
-
-        elif str(row[1]).strip() in journal_codes:
-            cleaned_rows_current_year.append({
-                'Num fournisseur': current_supplier_number,
-                'Nom fournisseur': current_supplier_name,
-                'journal': row[1],
-                'date': row[2].date() if pd.notna(row[2]) else None,
-                'N° de pièce': row[3],
-                'libelle': row[4],
-                'montant_debit': row[8],
-                'montant_credit': row[10],
-                'lettrage': row[9],
-                'Condition de paiement': current_payement_condition,
-            })
-
-    current_supplier_number = None
-    current_supplier_name = None
-    current_payement_condition = None
-
-    df_raw_past_year = pd.read_excel(past_year_file, header=None)
-    for _, row in df_raw_past_year.iterrows():
-        first_cell = str(row[0]).strip()
-
-
-        if first_cell.startswith('4411'):
-            current_supplier_number = first_cell.split()[0]
-            current_supplier_name = ' '.join(first_cell.split()[1:])
-            current_payement_condition = filter_supplier_condition(current_supplier_number)
-
-        elif str(row[1]).strip() in journal_codes:
-            cleaned_rows_past_year.append({
-                'Num fournisseur': current_supplier_number,
-                'Nom fournisseur': current_supplier_name,
-                'journal': row[1],
-                'date': row[2].date() if pd.notna(row[2]) else None,
-                'N° de pièce': row[3],
-                'libelle': row[4],
-                'montant_debit': row[8],
-                'montant_credit': row[10],
-                'lettrage': row[9],
-                'Condition de paiement': current_payement_condition,
-                  })
-
-    df_results_current_year = pd.DataFrame(cleaned_rows_current_year)
-    df_clean = df_results_current_year[~df_results_current_year['Num fournisseur'].isin(excluded_suppliers)]
-
-    df_results_past_year = pd.DataFrame(cleaned_rows_past_year)
-    df_clean_past = df_results_past_year[~df_results_past_year['Num fournisseur'].isin(excluded_suppliers)]
-    df_clean_past = df_clean_past[df_clean_past['journal'].isin(journal_achat)]
-
-    return df_clean, df_clean_past
 
 """Invoice from Trimester"""
 def query_1(df_clean, start_date, end_date):
@@ -277,3 +194,5 @@ def query_5(df_clean, df_clean_past, end_date, start_year):
     join.rename(columns={'N° de pièce_facture': 'N° de pièce'}, inplace=True)
 
     return join[join['date_paiement'].isna()]
+
+
---
+++ Ciel-React/api/run_query.py
@@ -0,0 +1,64 @@
+import datetime
+import pandas as pd
+import os 
+from queries import query_1, query_2, query_3, query_4, query_5
+from data_clean import clean_excel
+
+
+
+
+def month_delay(due_date, payment_date, end_date):
+    if pd.notna(payment_date):
+        return ((payment_date.year - due_date.year)*12 + (payment_date.month - due_date.month) + 1)
+    else:
+        return ((end_date.year - due_date.year)*12 + (end_date.month - due_date.month) +1 )
+    
+
+
+def run_query(current_year_file, past_year_files, excluded_suppliers, start, end_date, output_path='full_report_Q4.xlsx'):
+    start_str = datetime.datetime.strftime(start, '%Y-%m-%d')
+    start_date = datetime.datetime.strptime(start_str, '%Y-%m-%d').date() 
+    start_year = datetime.date(start_date.year, 1, 1)
+    df_clean, df_clean_past = clean_excel(current_year_file, past_year_files, excluded_suppliers)
+
+    q1 = query_1(df_clean, start_date, end_date)
+    q2 = query_2(df_clean, start_date, end_date, start_year)
+    q3 = query_3(df_clean, start_date, end_date, start_year)
+    q4 = query_4(df_clean, df_clean_past, start_date, end_date, start_year)
+    q5 = query_5(df_clean, df_clean_past, end_date, start_year)
+
+    result = pd.concat([q1, q2, q3, q4, q5])
+    result['Statut Paiement'] = result['date_paiement'].apply(lambda x: 'Réglée' if pd.notna(x) else 'Non Réglée')
+
+    result.drop(columns=['Num fournisseur', 'montant_debit','lettrage'],inplace=True)
+    result.rename(columns={'N° de pièce':'N° Facture'},inplace=True)
+    result.rename(columns={'montant_credit':'Montant Facture'},inplace=True)
+
+    result['mois_livraison'] = result['date_facture'].apply(
+        lambda x: x.strftime('%B-%Y').capitalize() if pd.notna(x) else None
+    )
+
+    result['date_paiement'] = pd.to_datetime(result['date_paiement'])
+    result['date_facture'] = pd.to_datetime(result['date_facture'])
+
+    result["date d'echeance"] = result['date_facture'] + pd.to_timedelta(result['Condition de paiement'], unit='D')
+
+
+    result['Delai paiement (jrs)'] = (result['date_paiement'] - result['date_facture']).dt.days.clip(lower=0)
+
+    result['Retard Jours'] = (result['Delai paiement (jrs)'] - result['Condition de paiement']).where(result['Delai paiement (jrs)'] > result['Condition de paiement'])
+    
+    result['Retard mois'] = result.apply(
+    lambda row: month_delay(row["date d'echeance"], row['date_paiement'], end_date) 
+    if (row['Retard Jours'] > 0) or (pd.isna(row['date_paiement']) and row["date d'echeance"] < pd.Timestamp(end_date)) 
+    else None, axis=1
+)
+    
+    result['date_paiement'] = result['date_paiement'].dt.date
+    result['date_facture'] = result['date_facture'].dt.date
+    result["date d'echeance"] = result["date d'echeance"].dt.date
+
+
+    result.to_excel(output_path, index=False)
+
+    os.startfile(output_path)
