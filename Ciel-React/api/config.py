0018c3ed28e2ec6ff18ea8656af2ff58634a5af6
Oussama <Hassouneoussama8@gmail.com>
2026-04-12 21:44:42 +1000

    config file logic

---
+++ Ciel-React/api/config.py
@@ -0,0 +1,34 @@
+
+import json
+
+
+DEFAULT_CONFIG = {
+    "journal_factures": [],
+    "journal_paiements": [],
+    "journal_report": [],
+    "condition_default": 60,
+    "conditions_fournisseur": [],
+    "fournisseurs_exclus": []
+}
+
+def load_config():
+
+    try:
+        with open("config.json") as file:
+            config_file = json.load(file)
+        return config_file
+    except FileNotFoundError:
+        with open('config.json', 'w') as f :
+            json.dump(DEFAULT_CONFIG, f, indent=4)
+        return DEFAULT_CONFIG
+    except json.JSONDecodeError:
+        with open('config.json', 'w') as f:
+            json.dump(DEFAULT_CONFIG, f, indent=4)
+        return DEFAULT_CONFIG
+
+
+def save_config(config):
+    with open('config.json', 'w') as f:
+        json.dump(DEFAULT_CONFIG, f, indent=4)
+
+
